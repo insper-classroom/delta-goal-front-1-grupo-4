4cec4d297d5e0ae0776f104fe069c3d36b26fa8e
Seu Nomeeduardadomingues <eduardapd@al.insper.edu.br>
2023-12-02 16:53:21 -0300

    lista

--- static/js/video.js
+++ static/js/video.js
@@ -1,32 +1,28 @@
 
   
+function seekToTime(seconds, jogadoresEnv, jogadoresDef) {
+    var video = document.getElementById('myVideo');
+    video.currentTime = seconds;
 
-function seekToTime(seconds, cruzamentoIndex) {
-    var video = document.getElementById("myVideo");
-    var newTime = seconds - 5; // Subtrai 5 segundos do tempo de cruzamento
-
-    video.currentTime = newTime >= 0 ? newTime : 0; // Evita definir um tempo negativo
-
-    var cruzamento = cruzamentos[cruzamentoIndex - 1]; // Ajuste conforme a estrutura de dados
-
-    // Atualizar lista de jogadores atacando
-    var listaAtacando = document.getElementById("jogadores-atacando-list");
-    listaAtacando.innerHTML = ''; // Limpar lista atual
-    cruzamento.nome_jogadores_time_cruzando.forEach(function(jogador) {
-        var li = document.createElement("li");
-        li.textContent = jogador;
+    // Atualiza a lista de jogadores atacando
+    var listaAtacando = document.getElementById('jogadores-atacando-list');
+    listaAtacando.innerHTML = ''; // Limpa a lista atual
+    jogadoresEnv.split(',').forEach(function(jogador) {
+        var li = document.createElement('li');
+        li.textContent = jogador.trim();
         listaAtacando.appendChild(li);
     });
 
-    // Atualizar lista de jogadores defendendo
-    var listaDefendendo = document.getElementById("jogadores-defendendo-list");
-    listaDefendendo.innerHTML = ''; // Limpar lista atual
-    cruzamento.nome_jogadores_time_defendendo.forEach(function(jogador) {
-        var li = document.createElement("li");
-        li.textContent = jogador;
+    // Atualiza a lista de jogadores defendendo
+    var listaDefendendo = document.getElementById('jogadores-defendendo-list');
+    listaDefendendo.innerHTML = ''; // Limpa a lista atual
+    jogadoresDef.split(',').forEach(function(jogador) {
+        var li = document.createElement('li');
+        li.textContent = jogador.trim();
         listaDefendendo.appendChild(li);
     });
 }
+
 
 
 
@@ -62,3 +58,6 @@ function playPause() {
         video.pause();
     }
 }
+
+
+
