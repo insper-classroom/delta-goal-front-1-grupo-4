b7b5bed83d2f818a4f01df60668e3f9ccf7029be
iancdesponds <iancdesponds@gmail.com>
2023-12-05 09:54:21 -0300

    feat: tira funções não usadas e adiciona filtros

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ from bson import json_util
 app = Flask('Delta_Goal_Front')
 app.config['SECRET_KEY'] = 'dg123'
 
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if 'username' not in session:
-            flash('Você precisa estar logado para acessar esta página.', 'warning')
-            return redirect(url_for('login'))
-        return f(*args, **kwargs)
-    return decorated_function
+@app.template_filter('convert_to_seconds')
+def convert_to_seconds(time_str):
+    hours, minutes, seconds = map(int, time_str.split(':'))
+    return hours * 3600 + minutes * 60 + seconds
+
+app.jinja_env.filters['convert_to_seconds'] = convert_to_seconds
+
+@app.template_filter('minus_five_seconds')
+def minus_five_seconds(seconds):
+    return max(seconds - 5, 0)
 
 @app.route("/")
 def redirect_to_login():
@@ -41,35 +43,17 @@ def sucesso_redefinicao():
     return render_template('sucesso.html')
 
 @app.route("/home")
-# @login_required
 def home():
     return render_template('inicial.html')
-
-def get_user_data(username):
-    user_data = session['username']
-
-    return user_data
-
-@app.route("/perfil")
-def perfil():
-    if 'username' in session:
-        username = session['username']
-        user_data = get_user_data(username)
-        if user_data:
-            return render_template('perfil.html', user_data=user_data)
-    
-    flash("Você precisa estar logado para acessar esta página.")
-    return redirect(url_for('login'))
-
 
 @app.route('/ver_dados')
 def ver_dados():
     return 'Dados'
 
 @app.route("/partida")
-# @login_required
 def partida():
-    return render_template('partida.html')
+    return render_template('partida2.html')
+    # return render_template('partida2.html', destaques=destaques, porcentagens=zone_percentages,porcentagens_bragan=zone_percentages_bragan,cruzamentos=cruzamentos_palmeiras)
     
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
