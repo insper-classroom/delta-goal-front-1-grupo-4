1813c07691e98dc5d6a3b5336ca1592df957638d
henriquemjf <ikejunqueira@gmail.com>
2023-12-07 17:09:00 -0300

    arrumei um erro

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def quebras():
     destaques = destaques.json()["destaques"]
     # quebras = requests.get("http://localhost:5000/quebra_linha")
     # quebras_palmeiras = quebras.json()["quebras_linha"]["pal"]
-    return render_template("quebras_de_linha.html",destaques=destaques
+    return render_template("quebras_de_linha.html",destaques=destaques)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
